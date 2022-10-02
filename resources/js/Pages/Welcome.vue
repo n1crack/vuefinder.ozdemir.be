f6ab89528bfb38c68e372c5e32d3408b726cc5b3
Yusuf <yusuf@ozdemir.be>
2022-10-02 17:47:26 +0300

    Initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <Head title="Vuefinder File Manager Example" />
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="w-full md:w-1/2">
+            <div class="text-gray-700 dark:text-white font-thin text-2xl text-center">Vuefinder File Library</div>
+
+            <div class="m-2 text-white font-thin text-lg text-center">
+                <a class="mx-3 text-emerald-500" href="https://github.com/n1crack/vuefinder">Github</a>
+                <a class="mx-3 text-emerald-500" href="https://www.npmjs.com/package/vuefinder">Npm</a>
+            </div>
+
+            <vue-finder id="vf" url="/vuefinder"></vue-finder>
+        </div>
+    </div>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+use Ozdemir\Vuefinder\Vuefinder;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\ReadOnly\ReadOnlyFilesystemAdapter;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+});
+
+Route::any('/vuefinder', function() {
+
+    $vuefinder = new VueFinder([
+        'local' => new ReadOnlyFilesystemAdapter(Storage::disk('local')->getAdapter()),
+        'media' =>  new ReadOnlyFilesystemAdapter(new  LocalFilesystemAdapter(dirname(__DIR__).'/storage/app/media/')),
+    ]);
+
+    $config = [
+        'publicLinks' => [
+            'local://public' => 'http://example.test',
+        ]
+    ];
+
+    $vuefinder->init($config);
+});
