2a5714ba9bd48f55609de2262ca7ea9bc1a3f26a
Yusuf Özdemir <yusuf@ozdemir.be>
2024-02-02 14:11:09 +0300

    update vuefinder

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,8 @@ import {createApp, h} from 'vue'
 import {createInertiaApp} from '@inertiajs/vue3'
 import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-import '../../../vuefinder/dist/style.css'
-import VueFinder from '../../../vuefinder/dist/vuefinder.js'
-
-// import 'vuefinder/dist/style.css'
-// import VueFinder from "vuefinder"
+import 'vuefinder/dist/style.css'
+import VueFinder from "vuefinder"
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -25,7 +22,22 @@ createInertiaApp({
     setup({el, App, props, plugin}) {
         createApp({render: () => h(App, props)})
             .use(plugin)
-            .use(VueFinder)
+            .use(VueFinder,
+                {
+                    i18n: {
+                        en:  () =>    import ("vuefinder/dist/locales/en.js"),
+                        fr:  () =>    import ("vuefinder/dist/locales/fr.js"),
+                        de:  () =>    import ("vuefinder/dist/locales/de.js"),
+                        fa:  () =>    import ("vuefinder/dist/locales/fa.js"),
+                        he:  () =>    import ("vuefinder/dist/locales/he.js"),
+                        hi:  () =>    import ("vuefinder/dist/locales/hi.js"),
+                        ru:  () =>    import ("vuefinder/dist/locales/ru.js"),
+                        sv:  () =>    import ("vuefinder/dist/locales/sv.js"),
+                        tr:  () =>    import ("vuefinder/dist/locales/tr.js"),
+                        zhCN:  () =>  import ("vuefinder/dist/locales/zhCN.js"),
+                        zhTW:  () =>  import ("vuefinder/dist/locales/zhTW.js"),
+                    }
+                })
             .use(ZiggyVue, Ziggy)
             .mount(el)
     },
